629a2d5392f128ba0875afd2540c8c033ceb61de
timothyjcollins <tcollins@greatergood.com>
2015-09-29 14:09:54 -0700

    upadte

--- fk_clicks_day.php
+++ fk_clicks_day.php
@@ -1,5 +1,5 @@
 <?php
-	$link = mysqli_connect("172.31.38.215", "freekibble", "freekibbleclick","freekibble");
+	$link = mysqli_connect("freekibble-click-counter.clltdiskvizr.us-west-2.rds.amazonaws.com", "freekibble", "freekibbleclick","freekibble");
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
